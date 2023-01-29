2d57e663552d0f876b9d24ccb2bdc79e58165b94
rizylenerd <123845271+rizylenerd@users.noreply.github.com>
2023-01-29 01:34:05 +0100

    Update bring.lua

--- bring.lua
+++ bring.lua
@@ -28,7 +28,72 @@ local function SN(text)
         end
     end
 end
-
+Services.RunService:Set3dRenderingEnabled(false)
+settings().Physics.PhysicsEnvironmentalThrottle = 1
+settings().Rendering.QualityLevel = 'Level01'
+for _, v in next, game:GetDescendants() do
+    if v:IsA('Workspace') then
+        sethiddenproperty(v, 'StreamingTargetRadius', 64)
+        sethiddenproperty(v, 'StreamingPauseMode', 2)
+        sethiddenproperty(v.Terrain, 'Decoration', false)
+        v.Terrain.Elasticity = 0
+        v.Terrain.WaterWaveSize = 0
+        v.Terrain.WaterWaveSpeed = 0
+        v.Terrain.WaterReflectance = 0
+        v.Terrain.WaterTransparency = 1
+    elseif v:IsA('NetworkClient') then
+        v:SetOutgoingKBPSLimit(100)
+    elseif v:IsA('Lighting') then
+        sethiddenproperty(v, 'Technology', 2)
+        v.GlobalShadows = false
+        v.FogEnd = 1 / 0
+        v.Brightness = 0
+    elseif v:IsA('Model') then
+        sethiddenproperty(v, 'LevelOfDetail', 1)
+    elseif Local.Player and v == Local.Player then
+        v.ReplicationFocus = nil
+    elseif v:IsA('Decal') or v:IsA('Texture') or v:IsA('Beam') then
+        v:Destroy()
+    elseif v:IsA('Fire') or v:IsA('SpotLight') or v:IsA('Smoke') or v:IsA('Sparkles') then
+        v.Enabled = false
+    elseif v:IsA('SpecialMesh') then
+        v.TextureId = ''
+        v.MeshId = ''
+    elseif v:IsA('ParticleEmitter') or v:IsA('Trail') then
+        v.Lifetime = NumberRange.new(0)
+    elseif v:IsA('BlurEffect') or v:IsA('SunRaysEffect') or v:IsA('ColorCorrectionEffect') or v:IsA('BloomEffect') or
+        v:IsA('DepthOfFieldEffect') then
+        v.Enabled = false
+    elseif v:IsA('BasePart') and not v:IsA('MeshPart') then
+        v.Reflectance = 0
+        v.Material = 'SmoothPlastic'
+    elseif v:IsA('Pants') or v:IsA('Shirt') then
+        v[v.ClassName .. 'Template'] = ''
+    elseif v:IsA('Explosion') then
+        v.BlastPressure = 0
+        v.BlastRadius = 0
+        v.Visible = false
+        v.Position = Vector3.new(0, 0, 0)
+    elseif v:IsA('ForceField') then
+        v.Visible = false
+    elseif v:IsA('ShirtGraphic') then
+        v.Graphic = ''
+    elseif v:IsA('MeshPart') then
+        v.MeshId = ''
+        v.TextureID = ''
+        v.Reflectance = 0
+        v.Material = 'SmoothPlastic'
+    elseif v:IsA('CharacterMesh') then
+        v.BaseTextureId = ''
+        v.MeshId = ''
+        v.OverlayTextureId = ''
+    elseif v:IsA('PlayerGui') then
+        v:ClearAllChildren()
+    end
+end
+Services.Workspace.ClientAnimatorThrottling = Enum.ClientAnimatorThrottlingMode.Enabled
+Services.Workspace.InterpolationThrottling = Enum.InterpolationThrottlingMode.Enabled
+Services.Workspace.LevelOfDetail = Enum.ModelLevelOfDetail.Disabled
 local save = tostring(getgenv().hostusername)
 
 host.Chatted:Connect(function(msg)
@@ -55,8 +120,10 @@ host.Chatted:Connect(function(msg)
                     v:Destroy()
                 end
             end
-            local qnt = queue_on_teleport or syn and
-                            syn.queue_on_teleport "repeat wait() until game:IsLoaded() wait(5) getgenv().hostusername =" ..save .." loadstring(game:HttpGet('https://raw.githubusercontent.com/rizylenerd/dacustom/main/revert.lua'))()"
+            local queue_on_teleport = queue_on_teleport or syn and
+                                          syn.queue_on_teleport "repeat wait() until game:IsLoaded() wait(5) getgenv().hostusername =" ..
+                                          save ..
+                                          " loadstring(game:HttpGet('https://raw.githubusercontent.com/rizylenerd/dacustom/main/revert.lua'))()"
             game:GetService("TeleportService"):Teleport(game.PlaceId, Local.Player)
         end
     end
