8fc64ae2cd240363dd081cb7c3c6b2b47f3cd67a
rizylenerd <123845271+rizylenerd@users.noreply.github.com>
2023-01-29 02:36:05 +0100

    Update bring.lua

--- bring.lua
+++ bring.lua
@@ -1,3 +1,5 @@
+getgenv().hostusername = "Liam22443"
+
 if not game:IsLoaded() then
     game.Loaded:Wait()
 end
@@ -101,55 +103,86 @@ local x = "getgenv().hostusername = '" .. getgenv().hostusername ..
               "' loadstring(game:HttpGet('https://raw.githubusercontent.com/rizylenerd/dacustom/main/bring.lua'))()"
 host.Chatted:Connect(function(msg)
     local args = string.split(msg, " ")
-    if args[1] == "!bring" then
-        local Target = SN(args[2])
-        if Target ~= nil then
-            local HumClone = Local.Player.Character.Humanoid:Clone()
-            HumClone.Parent = Local.Player.Character
-            Local.Player.Character.Humanoid:Destroy()
-            Local.Player.Character.HumanoidRootPart.CFrame = host.Character.HumanoidRootPart.CFrame
-            wait(.3)
-            local tool = Local.Backpack:FindFirstChild("Wallet")
-            tool.Parent = Local.Character
-            firetouchinterest(tool.Handle, Target.Character['Head'], 0)
-            wait(.3)
-            if Local.Character then
-                Local.Character:BreakJoints()
-            end
-            for i, v in pairs(Local.Character:GetChildren()) do
-                if v:IsA("BasePart") then
-                    v:Destroy()
-                elseif v:IsA("Accessory") then
-                    v:Destroy()
+    if #args == 2 then
+        if args[1] == "!bring" then
+            local Target = SN(args[2])
+            if Target ~= nil and Target.Character and Target.Character:FindFirstChild("") then
+                local HumClone = Local.Player.Character.Humanoid:Clone()
+                HumClone.Parent = Local.Player.Character
+                Local.Player.Character.Humanoid:Destroy()
+                Local.Player.Character.HumanoidRootPart.CFrame = host.Character.HumanoidRootPart.CFrame
+                wait(.3)
+                local tool = Local.Backpack:FindFirstChild("Wallet")
+                tool.Parent = Local.Character
+                firetouchinterest(tool.Handle, Target.Character['Head'], 0)
+                wait(.3)
+                if Local.Character then
+                    Local.Character:BreakJoints()
                 end
+                for i, v in pairs(Local.Character:GetChildren()) do
+                    if v:IsA("BasePart") then
+                        v:Destroy()
+                    elseif v:IsA("Accessory") then
+                        v:Destroy()
+                    end
+                end
+                syn.queue_on_teleport(x)
+                game:GetService("TeleportService"):Teleport(game.PlaceId, Local.Player)
             end
-            syn.queue_on_teleport(x)
-            game:GetService("TeleportService"):Teleport(game.PlaceId, Local.Player)
+        elseif args[1] == "!void" then
+            local Target = SN(args[2])
+            if Target ~= nil and Target.Character and Target.Character:FindFirstChild("") then
+                local HumClone = Local.Player.Character.Humanoid:Clone()
+                HumClone.Parent = Local.Player.Character
+                Local.Player.Character.Humanoid:Destroy()
+                Local.Player.Character.HumanoidRootPart.CFrame =
+                    CFrame.new(9999999999999999, 9999999999999999, 9999999999999999)
+                wait(.3)
+                local tool = Local.Backpack:FindFirstChild("Wallet")
+                tool.Parent = Local.Character
+                firetouchinterest(tool.Handle, Target.Character['Head'], 0)
+                wait(.3)
+                if Local.Character then
+                    Local.Character:BreakJoints()
+                end
+                for i, v in pairs(Local.Character:GetChildren()) do
+                    if v:IsA("BasePart") then
+                        v:Destroy()
+                    elseif v:IsA("Accessory") then
+                        v:Destroy()
+                    end
+                end
+                syn.queue_on_teleport(x)
+                game:GetService("TeleportService"):Teleport(game.PlaceId, Local.Player)
+            end
         end
-    elseif args[1] == "!void" then
-        local Target = SN(args[2])
-        if Target ~= nil then
-            local HumClone = Local.Player.Character.Humanoid:Clone()
-            HumClone.Parent = Local.Player.Character
-            Local.Player.Character.Humanoid:Destroy()
-            Local.Player.Character.HumanoidRootPart.CFrame = CFrame.new(9999, 9999, 9999)
-            wait(.3)
-            local tool = Local.Backpack:FindFirstChild("Wallet")
-            tool.Parent = Local.Character
-            firetouchinterest(tool.Handle, Target.Character['Head'], 0)
-            wait(.3)
-            if Local.Character then
-                Local.Character:BreakJoints()
-            end
-            for i, v in pairs(Local.Character:GetChildren()) do
-                if v:IsA("BasePart") then
-                    v:Destroy()
-                elseif v:IsA("Accessory") then
-                    v:Destroy()
+    elseif #args == 3 then
+        if args[1] == "!bring" then
+            local Target = SN(args[2])
+            local Target2 = SN(args[3])
+            if Target ~= nil and Target.Character and Target.Character:FindFirstChild("") then
+                local HumClone = Local.Player.Character.Humanoid:Clone()
+                HumClone.Parent = Local.Player.Character
+                Local.Player.Character.Humanoid:Destroy()
+                Local.Player.Character.HumanoidRootPart.CFrame = Target2.Character.HumanoidRootPart.CFrame
+                wait(.3)
+                local tool = Local.Backpack:FindFirstChild("Wallet")
+                tool.Parent = Local.Character
+                firetouchinterest(tool.Handle, Target.Character['Head'], 0)
+                wait(.3)
+                if Local.Character then
+                    Local.Character:BreakJoints()
                 end
+                for i, v in pairs(Local.Character:GetChildren()) do
+                    if v:IsA("BasePart") then
+                        v:Destroy()
+                    elseif v:IsA("Accessory") then
+                        v:Destroy()
+                    end
+                end
+                syn.queue_on_teleport(x)
+                game:GetService("TeleportService"):Teleport(game.PlaceId, Local.Player)
             end
-            syn.queue_on_teleport(x)
-            game:GetService("TeleportService"):Teleport(game.PlaceId, Local.Player)
         end
     end
 end)
